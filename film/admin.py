a73e71702c942aabb9f5293847cfddc36cf37d29
Cathal <cathalcrowepatrick@gmail.com>
2024-11-09 19:58:41 +0000

    Removal of film app (no need)

--- film/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Film
-
-@admin.register(Film)
-class FilmAdmin(admin.ModelAdmin):
-    list_display = ("title", "genre", "release_date", "director")
--- film/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Film
-
-def film_list(request):
-    films = Film.objects.all()
-    return render(request, "film_list.html", {"films": films})
