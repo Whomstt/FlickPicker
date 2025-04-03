2cf68e6a7ba5709352abf455f144a8372b9a0365
Cathal <cathalcrowepatrick@gmail.com>
2025-04-03 17:29:05 +0100

    65k yolks

--- chatbot/config.py
+++ chatbot/config.py
@@ -26,15 +26,15 @@ OLLAMA_URL = "http://ollama:11434/api"
 EMBEDDING_MODEL = "nomic-embed-text"
 
 # FAISS and embedding parameters
-NPROBE = 1  # Number of clusters to be searched
-NLIST = 1  # Number of clusters to be stored
+NPROBE = 32  # Number of clusters to be searched
+NLIST = 256  # Number of clusters to be stored
 M = 16  # Number of subquantizers
-NBITS = 4  # Number of bits per subquantizer
+NBITS = 8  # Number of bits per subquantizer
 
 N_TOP_MATCHES = 5  # Number of top matches to return
 SEARCH_INCREMENT = 8192  # Increment for searching more results
 MAX_RESULTS = 65536  # Maximum number of search results
-NPROBE_INCREMENT = 128  # Increment for nprobe
+NPROBE_INCREMENT = 32  # Increment for nprobe
 
 PROMPT_WEIGHT = 1  # Weight for prompt embedding
 NAME_WEIGHT = 0.6  # Weight for actor / director names embedding
@@ -46,12 +46,12 @@ KEYWORD_WEIGHT = 0.4  # Weight for keyword embedding
 NAME_FUZZY_THRESHOLD = 90  # Fuzzy matching threshold for names in %
 GENRE_FUZZY_THRESHOLD = 80  # Fuzzy matching threshold for genres in %
 TITLE_FUZZY_THRESHOLD = 90  # Fuzzy matching threshold for titles in %
-KEYWORD_FUZZY_THRESHOLD = 80  # Fuzzy matching threshold for keywords in %
+KEYWORD_FUZZY_THRESHOLD = 90  # Fuzzy matching threshold for keywords in %
 
 # Settings for TMDB API
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")  # API key from environment variables
-TMDB_NUM_FILMS = 10  # Number of films to fetch per year
+TMDB_NUM_FILMS = 1024  # Number of films to fetch per year
 TMDB_RATE_LIMIT = 50  # TMDB rate limit 50 requests per second
 TMDB_RATE_LIMIT_WINDOW = 1  # Rate limit window in seconds
 TMDB_TOTAL_PAGES = 500  # Total number of pages available
